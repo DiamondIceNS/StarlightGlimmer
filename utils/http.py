e4ae8f2913fcac1dc64c279d0b2c2367cd6a971c
DiamondIceNS <fawfulcopter@gmail.com>
2019-09-08 09:52:13 -0500

    Add proper rate limiter for Pixelzone

--- utils/http.py
+++ utils/http.py
@@ -1,4 +1,5 @@
 import asyncio
+from collections import deque
 import io
 import json
 import logging
@@ -16,7 +17,11 @@ from utils.version import VERSION
 
 log = logging.getLogger(__name__)
 
-useragent = {'User-Agent': 'StarlightGlimmer/{} (+https://github.com/DiamondIceNS/StarlightGlimmer)'.format(VERSION), 'Cache-Control': 'no-cache'}
+useragent = {
+    'User-Agent': 'StarlightGlimmer/{} (+https://github.com/DiamondIceNS/StarlightGlimmer)'.format(VERSION),
+    'Cache-Control': 'no-cache'
+}
+pz_rate_limiter = None
 
 
 async def fetch_chunks(chunks: Iterable):
@@ -55,6 +60,9 @@ async def _fetch_chunks_pixelcanvas(bigchunks: Iterable[BigChunk]):
 
 
 async def _fetch_chunks_pixelzone(chunks: Iterable[ChunkPz]):
+    global pz_rate_limiter
+    if pz_rate_limiter is None:
+        pz_rate_limiter = RateLimitingSemaphore(2, 0.4, asyncio.get_event_loop())
     socket_url = "{0}://pixelzone.io/socket.io/?EIO=3&transport={1}"
     sid = None
     chunks = [x for x in chunks if x.is_in_bounds()]
@@ -97,23 +105,23 @@ async def _fetch_chunks_pixelzone(chunks: Iterable[ChunkPz]):
             async for msg in ws:
                 if msg[4:18] == 'useAPICallback':
                     break
-            for ch in chunks:
-                await ws.send(ch.url)
-                packet = None
-                async for msg in ws:
-                    if packet is None:
-                        if msg[:2] == '45':
-                            packet = {}
-                            data = json.loads(msg[msg.find('-')+1:])[1]
-                            packet['cx'] = data['cx']
-                            packet['cy'] = data['cy']
-                    else:
-                        packet['data'] = msg[1:]
-                        break
-                ch = next((x for x in chunks if x.x == packet['cx'] and x.y == packet['cy']))
-                ch.load(packet['data'])
-                await asyncio.sleep(0.4)  # avoid the rate limit
-        except websockets.ConnectionClosed as e:
+            async with pz_rate_limiter:
+                for ch in chunks:
+                    await ws.send(ch.url)
+                    packet = None
+                    async for msg in ws:
+                        if packet is None:
+                            if msg[:2] == '45':
+                                packet = {}
+                                data = json.loads(msg[msg.find('-')+1:])[1]
+                                packet['cx'] = data['cx']
+                                packet['cy'] = data['cy']
+                        else:
+                            packet['data'] = msg[1:]
+                            break
+                    ch = next((x for x in chunks if x.x == packet['cx'] and x.y == packet['cy']))
+                    ch.load(packet['data'])
+        except websockets.ConnectionClosed:
             raise HttpCanvasError('pixelzone')
 
 
@@ -200,7 +208,7 @@ async def fetch_online_pixelzone():
                     d = json.loads(msg[2:])
                     data = d[1]
                     break
-        except websockets.ConnectionClosed as e:
+        except websockets.ConnectionClosed:
             raise HttpCanvasError('pixelzone')
     return data
 
@@ -242,3 +250,32 @@ async def get_template(url, name):
             if resp.content_type != "image/png":
                 raise NotPngError
             return io.BytesIO(await resp.read())
+
+
+# https://stackoverflow.com/a/46255794
+class RateLimitingSemaphore:
+    def __init__(self, max_reqs, interval, loop):
+        self.loop = loop
+        self.max_reqs = max_reqs
+        self.interval = interval / max_reqs
+        self.queued_calls = 0
+        self.call_times = deque()
+
+    async def __aenter__(self):
+        self.queued_calls += 1
+        while True:
+            cur_rate = 0
+            if len(self.call_times) == self.max_reqs:
+                cur_rate = self.max_reqs / (self.loop.time() - self.call_times[0])
+            if cur_rate < self.max_reqs:
+                break
+            elapsed_time = self.loop.time() - self.call_times[-1]
+            await asyncio.sleep(self.queued_calls * self.interval - elapsed_time)
+        self.queued_calls -= 1
+
+        if len(self.call_times) == self.max_reqs:
+            self.call_times.popleft()
+        self.call_times.append(self.loop.time())
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
